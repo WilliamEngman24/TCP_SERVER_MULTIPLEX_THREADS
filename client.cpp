06677b043395c2c16f69f8e465d40f716f137f98
WilliamEngman24 <williamengman24@gmail.com>
2025-06-13 10:58:10 +0200

    it works... YAHOOOO

---
+++ client.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define IP "127.0.0.1"
+#define BUFFER_SIZE 1024
+
+using namespace std;
+
+int main()
+{
+    //create client socket
+    int client_fd = socket (AF_INET, SOCK_STREAM, 0);
+    if(client_fd < 0)
+    {
+        cout << "Failed to create client socket" << endl;
+        close(client_fd);
+        return 0;
+    }
+
+    //configuring server address
+    struct sockaddr_in server;
+    memset(&server, 0 , sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+
+    //setting server address
+    if (inet_pton(AF_INET, IP, &server.sin_addr) <= 0) 
+    {
+        cout << "Failed to assign IP address" << endl;
+        close(client_fd);
+        return 0;
+    }
+
+    //connect client socket to server address
+    if (connect(client_fd, (struct sockaddr*)&server, sizeof(server)) < 0) 
+    {
+        cout << "failed to connect to server" << endl;
+        close(client_fd);
+        return 0;
+    }
+
+    //enter into constant feed
+    bool connection = true;
+    while(connection) 
+    {
+        string userInput = "Data";
+
+        //send first message to server
+        int status = send(client_fd,userInput.c_str(), userInput.size(), 0);
+        if (status < 0) 
+        {
+            cout << "Failed to send first message" << endl;
+        }
+        else 
+        {
+            //create buffer for in comming message from server
+            char buffer[BUFFER_SIZE];
+            memset(buffer, 0, BUFFER_SIZE); // zero-initilaizing
+
+            int bytes = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+            if (bytes <= 0) 
+            {
+                cout << "No data received!" << endl;
+            }
+            else 
+            {
+                string message(buffer, bytes); //takes buffer and bytes to make a new string
+                cout << message << endl; //print server response
+            }
+        }
+
+    }
+    close(client_fd);
+}
--- robust_server.cpp
+++ robust_server.cpp
@@ -6,6 +6,7 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <unordered_map>
 
 #include <unistd.h>
 #include <sys/socket.h>
@@ -17,7 +18,7 @@
 #define PORT 8080
 #define IP "127.0.0.1"
 #define BUFFER_SIZE 1024
-#define THREAD_POOL_SIZE 4
+#define THREAD_POOL 4
 
 using namespace std;
 using namespace chrono;
@@ -30,23 +31,19 @@ class ClientState
     string buffer;
     steady_clock::time_point last_activity; //last time used
 
-    //constructor
-    clientState(int new_fd) 
-    : fd(new_fd), buffer(""), last_activity(steady_clock::now()) {} //when creating new object take time now
-    
     public:
-    int get_fd() 
-    {
-        return fd;
-    }
+
+    //default constructor
+    ClientState() 
+    : fd(-1), buffer(""), last_activity(steady_clock::now()) {}
+
+    //constructor
+    ClientState(int new_fd) 
+    : fd(new_fd), buffer(""), last_activity(steady_clock::now()) {} //when creating new object take time now
+
     void update_activity() 
     {
         last_activity = steady_clock::now();
-    }
-
-    void close_client()
-    {
-        close(fd);
     }
 };
 
@@ -54,7 +51,7 @@ class ClientState
 void non_blocking_fd(int fd) 
 {
     //Current flags
-    int flags = fcntl(fd, F_GETL, 0); 
+    int flags = fcntl(fd, F_GETFL, 0); 
 
     //set the requested fd to non-blocking
     fcntl(fd, F_SETFL, flags | O_NONBLOCK);
@@ -71,19 +68,19 @@ struct Task
 //used to share work tasks between threads
 class TaskQueue 
 {
-    queue<Task> queue; //task container
+    queue<Task> task_queue; //task container
     mutex mutex_Var; //synchronize access
     condition_variable condition; //block worker threads
 
     public:
-    push(Task& task) 
+    void push(Task task) 
     {
         { // extra braces to ensure unlock before notifying
 
             //ensures exclusive access to queue.
             lock_guard<mutex> lock(mutex_Var); //prevents data races.
         
-            queue.push(task); //adds new task to front of queue
+            task_queue.push(task); //adds new task to front of queue
         }
 
         //wake up/notify a waiting thread
@@ -97,13 +94,13 @@ class TaskQueue
         unique_lock<mutex> lock(mutex_Var);
 
         //puts thread to sleepuntil condition met
-        condition.wait(lock, [&]() {return !queue.empty();});
+        condition.wait(lock, [&]() {return !task_queue.empty();});
         
         //get task from front
-        Task task = queue.front();
+        Task task = task_queue.front();
 
         //removes front most task
-        queue.pop();
+        task_queue.pop();
 
         //returns task recently removed
         return task;
@@ -112,7 +109,7 @@ class TaskQueue
 
 //Manages a number of worker threads
 //Pulls tasks from a shared synchronized queue
-class ThreahPool 
+class ThreadPool 
 {
     vector<thread> workers; //worker threads
 
@@ -127,7 +124,7 @@ class ThreahPool
     : task_queue(new_task_queue) 
     {
         //launch each worker thread
-        for(int i = 0; i < THREAD_POOL_SIZE; ++i) 
+        for(int i = 0; i < THREAD_POOL; ++i) 
         {
             //adds new thread to workers
             //"this" isnthe ThreadPool instance, runs a lambda
@@ -136,25 +133,40 @@ class ThreahPool
                 //worker thread loop
                 while(!stop) 
                 {
-
+                    //removes front most task and returns it
                     Task task = task_queue.pop();
+
+                    //the message sent to client with task data
                     string response = "Echo: " + task.data;
-                    send(task.client_fd, response.c_str(), response.size(), 0);
+
+                    //send said message to client 
+                    int status = send(task.client_fd, response.c_str(), response.size(), 0); 
+                    {
+                        if(status <0) 
+                        {
+                            cout << "Failed to send echo to client, in thread pool" << endl;
+                        }
+                    }
                 }
             });
         }
     }
 
-    //deconstructor
+    //destructor
+    //shuts down all worker threads
     ~ThreadPool() 
     {
-        stop = true;
-        for (int i = ; i < THREAD_POOL_SIZE; ++i) 
+        stop = true; //will exit loop after exiting this
+
+        //unblock all threads that might be stuck on pop()
+        for (int i = 0; i < THREAD_POOL; ++i) 
         {
+            //signals worker thread to exit
             task_queue.push({-1, ""});
         }
         for(auto& worker : workers)
         {
+            //waits for all threads to join, no threads left running 
             worker.join();
         }
     }
@@ -177,7 +189,7 @@ int main()
     int optval = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
-    //make server file descriptor to non-blobking
+    //make server file descriptor to non-blocking
     non_blocking_fd(server_fd);
 
     //zero-initializing socket adress
@@ -196,7 +208,7 @@ int main()
         return 0;
     }
 
-    //put pocket on listen mode
+    //put socket on listen mode
     if (listen(server_fd, 5) < 0)
     {
         cout << "Failed to listen to socket" << endl;
@@ -209,9 +221,11 @@ int main()
     // I/O multiplexing setup using poll
 
     vector<pollfd> fds; // vector of poll file descriptors
-    unordered_map<int, string> cl
-    fds.pushback({server_fd, POLLIN, 0}); // puts the server_fd in the vector to be monitored
+    unordered_map<int, ClientState> clients;
+    fds.push_back({server_fd, POLLIN, 0}); // puts the server_fd in the vector to be monitored
 
+    TaskQueue task_queue;
+    ThreadPool pool(task_queue);
 
     while(1) 
     {
@@ -220,22 +234,27 @@ int main()
 
         if(poll_status < 0) 
         {
-            cout "Failed to poll" << endl;
+            cout << "Failed to poll" << endl;
+            break;
         }
+
+        steady_clock::time_point now = steady_clock::now();
 
         //Proccess new connections and incoming data
         for (size_t i = 0; i < fds.size(); ++i) // Iterate over all fds,
         {
+            //Extracts file descriptor from vector of a poll that has all file descriptors
+            int fd = fds[i].fd;
             if (fds[i].revents & POLLIN) // checks if specific fd is ready to read
             {
                 if(fds[i].fd == server_fd) //if a new connection has been made on the server socket
                 {
                     //create client
-                    sockaddr_in client;
-                    socklen_t client_len = sizeof(client);
+                    sockaddr_in client_addr;
+                    socklen_t client_len = sizeof(client_addr);
 
                     //accept new client
-                    int client_fd = accept(server_fd, (sockaddr*)client, client_len);
+                    int client_fd = accept(server_fd, (sockaddr*)&client_addr, &client_len);
                     if(client_fd < 0) 
                     {
                         cout << "Failed to accept client" << endl;
@@ -247,16 +266,43 @@ int main()
                     //add new client to file descriptors on ready to read
                     fds.push_back({client_fd, POLLIN, 0});
 
+                    //adds new client to unordered map
+                    clients.emplace(client_fd, ClientState(client_fd));
+
                     cout << "A new client has connected: " << client_fd << endl;
                 }
-                else //client sent data 
+                else //handle dlient data 
                 {
-                    //current index in fds assigns it's file descriptor to variable 
-                    int client_fd = fds[i].fd;
-                    
+                    char buffer[BUFFER_SIZE];
+                    int bytes_read = recv(fd, buffer, sizeof(buffer), 0);
+                    if(bytes_read <= 0 ) //data was not received
+                    {
+                        cout << "Failed to receive from client: " << fd << endl;
+                        
+                        //closes client socket and removes it from clients map and fds vector
+                        close(fd);
+                        clients.erase(fd);
+                        fds.erase(fds.begin() + i);
+                        --i; //decrement so loop doesn't skip next element after erasing
+                    }
+                    else //data was received
+                    {
+                        //assigns data from clients map to new object
+                        auto&client = clients[fd];
+
+                        //updates activity of client
+                        client.update_activity();
+
+                        //
+                        string data(buffer, bytes_read);
+                        task_queue.push({fd, data});
+                    }
                 }
             }
         }
+
+        // Idle client removal here
+
     }
 
     close(server_fd);
